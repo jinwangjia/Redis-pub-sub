26522b77bffb1a8fd92237e6bfb9612bd650964c
wangjia <jin_wangjia@163.com>
2019-10-24 16:01:44 +0800

    添加项目文件。

---
+++ ConsoleAppPub/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            var sub = redis.GetSubscriber();
+
+            sub.Publish("Channel1", "dsdsdsds");
+        }
+    }
+}
---
+++ ConsoleAppSub/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ConsoleAppSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            //var db = redis.GetDatabase();
+            var sub = redis.GetSubscriber();
+            sub.Subscribe("Channel1", (channel, message) =>
+            {
+                Console.WriteLine($"Channel1{channel.ToString()} 订阅收到消息:{message.ToString()}");
+            });
+            Console.ReadLine();
+        }
+    }
+}
